a8b93b2a62c59b5a230e30c3ad3486b1f24a74ee
muhd1122 <muhammadsaad3325@gmail.com>
2026-07-14 06:56:02 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,37 @@
+const text = [
+  "Computer Science Student",
+  "Frontend Web Developer",
+  "UI/UX Designer",
+  "Freelancer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === text.length) {
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500);
+  } else {
+    setTimeout(type, 100);
+  }
+})();
+window.addEventListener("load", function () {
+    const audio = document.getElementById("welcomeAudio");
+
+    audio.play().catch(() => {
+        console.log("Autoplay was blocked by the browser.");
+    });
+});
